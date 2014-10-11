9bca3079a0971ec7efafaa7f3f6317adabe4d977
Henry Szeto <ucla@hotmail.com>
2014-10-11 15:00:26 -0700

    add win logic to js

--- js/main.js
+++ js/main.js
@@ -13,17 +13,65 @@ $scope.board = [
 //				};
 
 $scope.moveCounter = 0;
+$scope.xPoint = 0;
+$scope.oPoint = 0;
 
 $scope.playerPicks = function(thisCell){
-	$scope.moveCounter = $scope.moveCounter + 1;
-	console.log("Cell was: " + thisCell.status);
-	if (($scope.moveCounter %2) == 1){
-		thisCell.status = "X";
-	} else {
-		thisCell.status = "O";
-	}
-	console.log("Cell is now: " + thisCell.status);
-};
+		
+		console.log("Cell was: " + thisCell.status);
 
+	while (thisCell.status == "Blank"){
+		$scope.moveCounter = $scope.moveCounter + 1;
+			if (($scope.moveCounter % 2) != 0){
+				thisCell.status = "X";
+				$scope.xPoint = $scope.xPoint + thisCell.pt ;
+			} else if (($scope.moveCounter % 2) == 0){
+				thisCell.status = "O";
+				$scope.oPoint = $scope.oPoint + thisCell.pt ;
+			} 
+
+			switch($scope.xPoint){
+				case 7 : thisCell.status = "X WINS!"; break;
+				case 56 : thisCell.status = "X WINS!"; break;
+				case 73 : thisCell.status = "X WINS!"; break;
+				case 84 : thisCell.status = "X WINS!"; break;
+				case 146: thisCell.status = "X WINS!"; break;
+				case 273: thisCell.status = "X WINS!"; break;
+				case 296: thisCell.status = "X WINS!"; break;
+				case 448: thisCell.status = "X WINS!"; break;
+				default: console.log("keep going");
+			};
+			switch($scope.oPoint){
+				case 7  : thisCell.status = "O WINS!"; break;
+				case 56 : thisCell.status = "O WINS!"; break;
+				case 73 : thisCell.status = "O WINS!"; break;
+				case 84 : thisCell.status = "O WINS!"; break;
+				case 146: thisCell.status = "O WINS!"; break;
+				case 273: thisCell.status = "O WINS!"; break;
+				case 296: thisCell.status = "O WINS!"; break;
+				case 448: thisCell.status = "O WINS!"; break;
+				default: console.log("keep going");
+			};
+
+			if ($scope.moveCounter == 9) {
+				thisCell.status = "Game over.";
+			}
+
+	} // while loop
+
+}; // function playerPicks
+
+
+
+//$scope.playerPicks = function(thisCell){
+//	$scope.moveCounter = $scope.moveCounter + 1;
+//	console.log("Cell was: " + thisCell.status);
+//	if ((thisCell.status == "Blank") && ($scope.moveCounter %2) == 0){
+//		thisCell.status = "X";
+//	} else if ((thisCell.status == "Blank") && ($scope.moveCounter %2) == 1){
+//		thisCell.status = "O";
+//	}
+//	console.log("Cell is now: " + thisCell.status);
+//};
 
 }); // end of TTTController
