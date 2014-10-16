60340d39ec5d6ce194e677bba189f0001056420b
Henry Szeto <ucla@hotmail.com>
2014-10-16 14:42:37 -0700

    new working win logic with binary code

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ $scope.oPoint = 0;
 $scope.playerPicks = function(thisCell){										// function playerPicks starts 
 	while ((thisCell.status == "Blank") && ($scope.gameStatus=="Game On!")) {	// check for blank cells and game status is on.
 		$scope.moveCounter = $scope.moveCounter + 1;							// increase move counter
+
 			if (($scope.moveCounter % 2) != 0){									// if move counter is odd, then it's X's move.
 				thisCell.status = "X";				
 				thisCell.image = "images/bigX.png";
@@ -26,33 +27,39 @@ $scope.playerPicks = function(thisCell){										// function playerPicks starts
 				$scope.oPoint = $scope.oPoint + thisCell.pt ;					// increase O points.
 			} 
 
-			switch($scope.xPoint){												// Win logic. Case match X points.
-				case 7 : thisCell.status = "X WINS!"; $scope.moveCounter == 9; $scope.xWin(); break;
-				case 56 : thisCell.status = "X WINS!"; $scope.moveCounter == 9; $scope.xWin(); break;
-				case 73 : thisCell.status = "X WINS!"; $scope.moveCounter == 9; $scope.xWin(); break;
-				case 84 : thisCell.status = "X WINS!"; $scope.moveCounter == 9; $scope.xWin(); break;
-				case 146: thisCell.status = "X WINS!"; $scope.moveCounter == 9; $scope.xWin(); break;
-				case 273: thisCell.status = "X WINS!"; $scope.moveCounter == 9; $scope.xWin(); break;
-				case 292: thisCell.status = "X WINS!"; $scope.moveCounter == 9; $scope.xWin(); break;
-				case 448: thisCell.status = "X WINS!"; $scope.moveCounter == 9; $scope.xWin(); break;
-				default: console.log("keep going");
-			};
-			switch($scope.oPoint){												// Win logic. Case match O points.
-				case 7  : thisCell.status = "O WINS!"; $scope.moveCounter == 9; $scope.oWin(); break;
-				case 56 : thisCell.status = "O WINS!"; $scope.moveCounter == 9; $scope.oWin(); break;
-				case 73 : thisCell.status = "O WINS!"; $scope.moveCounter == 9; $scope.oWin(); break;
-				case 84 : thisCell.status = "O WINS!"; $scope.moveCounter == 9; $scope.oWin(); break;
-				case 146: thisCell.status = "O WINS!"; $scope.moveCounter == 9; $scope.oWin(); break;
-				case 273: thisCell.status = "O WINS!"; $scope.moveCounter == 9; $scope.oWin(); break;
-				case 292: thisCell.status = "O WINS!"; $scope.moveCounter == 9; $scope.oWin(); break;
-				case 448: thisCell.status = "O WINS!"; $scope.moveCounter == 9; $scope.oWin(); break;
-				default: console.log("keep going");
+			var winPoint = [7,56,73,84,146,273,292,448];						// 8 possible winning points in array.
+
+			for (var i=0; i<8; i++) {
+				switch(winPoint[i] & $scope.xPoint){							// Win logic. Case match X points.
+					case 7 : thisCell.status = "X WINS!"; $scope.xWin(); break;
+					case 56 : thisCell.status = "X WINS!"; $scope.xWin(); break;
+					case 73 : thisCell.status = "X WINS!"; $scope.xWin(); break;
+					case 84 : thisCell.status = "X WINS!"; $scope.xWin(); break;
+					case 146 : thisCell.status = "X WINS!"; $scope.xWin(); break;
+					case 273 : thisCell.status = "X WINS!"; $scope.xWin(); break;
+					case 292 : thisCell.status = "X WINS!"; $scope.xWin(); break;
+					case 448 : thisCell.status = "X WINS!"; $scope.xWin(); break;
+					default: console.log("keep going");
+				};
+			
+				switch(winPoint[i] & $scope.oPoint){							// Win logic. Case match O points.
+					case 7  : thisCell.status = "O WINS!"; $scope.oWin(); break;
+					case 56 : thisCell.status = "O WINS!"; $scope.oWin(); break;
+					case 73 : thisCell.status = "O WINS!"; $scope.oWin(); break;
+					case 84 : thisCell.status = "O WINS!"; $scope.oWin(); break;
+					case 146: thisCell.status = "O WINS!"; $scope.oWin(); break;
+					case 273: thisCell.status = "O WINS!"; $scope.oWin(); break;
+					case 292: thisCell.status = "O WINS!"; $scope.oWin(); break;
+					case 448: thisCell.status = "O WINS!"; $scope.oWin(); break;
+					default: console.log("keep going");
+				};
 			};
 
-			if ($scope.moveCounter == 9) {										// fire gameover function when max move is reached.
-				$scope.gameOver()
+			 if ($scope.moveCounter == 9) {										// fire gameover function when max move is reached.
+				$scope.gameOver();
 				break;
 			}
+
 	} // while loop ends
 }; // function playerPicks ends
 
